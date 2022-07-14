30a498652f1275fd9fa52b98c06c1ac613e1be64
Doug Martin <dmartin@concord.org>
2022-07-13 21:15:33 -0400

    Changed get-contents to use cursor

--- scripts/docstore-get-contents.ts
+++ scripts/docstore-get-contents.ts
@@ -5,8 +5,10 @@ import readJSON from "./read-json";
 import writeFile from "./write-file";
 import { Client } from "pg"
 import { writeDocstoreFile } from "./docstore-file";
+import Cursor from "pg-cursor"
+import { promisify } from "util";
 
-const docStoreGetContents = (env: string, docStoreConfig: DocStoreConfig) => {
+const docStoreGetContents = async (env: string, docStoreConfig: DocStoreConfig) => {
   const {dbHost, dbPassword, dbUser, dbDatabase} = docStoreConfig;
   
   const docStoreSizes = readJSON(env, "docstore-sizes.json")
@@ -30,20 +32,28 @@ const docStoreGetContents = (env: string, docStoreConfig: DocStoreConfig) => {
   })
 
   log("Connecting to docstore database")
-  client.connect()
-    .then(() => {
-      log("Querying for docstore documents to get contents")
-      return client.query(sql)
+  await client.connect()
+  log("Querying for docstore documents to get contents")
+
+  const cursor = client.query(new Cursor(sql))
+  const promisifiedCursorRead = promisify(cursor.read.bind(cursor));
+
+  while (true) {
+    log("Reading up to 100 documents")
+    const rows = await promisifiedCursorRead(100);
+    log(`Read ${rows.length} documents`)
+    if (rows.length === 0) {
+      break;
+    }
+
+    rows.forEach(row => {
+      writeDocstoreFile(env, row.document_id, row.content)
     })
-    .then((result) => {
-      log(`Found ${result.rows.length} results in docstore database`)
-      result.rows.forEach(row => {
-        writeDocstoreFile(env, row.document_id, row.content)
-      })
-      writeFile(env, "docstore-contents.json", result.rows)
-    })
-    .then(() => client.end())
-    .catch(err => die(err))
+  }  
+
+  cursor.close(() => {
+    client.end();
+  });
 };
 
 export default docStoreGetContents;
